6b69aff33ba369e5a366df799b8cdba1e819d5b0
Diana0604 <diana.vallverdu@gmail.com>
2022-11-15 08:31:23 +0000

    baseUrl as variable

--- api/database.ts
+++ api/database.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
+import config from "../config/config.index";
+
+const databaseUrl = `${config.baseUrl}/mysql`;
 
 export const restartDatabase = async (
   setWarningOn: (arg0: boolean) => void
 ) => {
   try {
-    await axios.post("http://localhost:3000/mysql/restart");
+    await axios.post(`${databaseUrl}/restart`);
     setWarningOn(false);
   } catch (error) {
     console.log("error restart db");
@@ -15,7 +18,7 @@ export const restartDatabase = async (
 
 export const getCompanies = async () => {
   try {
-    const res = await axios.get("http://localhost:3000/mysql/companies");
+    const res = await axios.get(`${databaseUrl}/companies`);
     return res.data;
   } catch (error) {
     console.log("error getting companies back");
--- api/showManagement.ts
+++ api/showManagement.ts
@@ -1,20 +1,23 @@
 import axios from "axios";
+import config from "../config/config.index";
+
+const showUrl = `${config.baseUrl}/show`;
 
 export const playShow = async (setPlaying: (arg0: boolean) => void) => {
   try {
-    await axios.post('http://localhost:3000/show/play');
-    setPlaying(true)
-  } catch(error) {
-    console.log(`error asking api to play show`)
+    await axios.post(`${showUrl}/play`);
+    setPlaying(true);
+  } catch (error) {
+    console.log(`error asking api to play show`);
   }
 };
 
 export const pauseShow = async (setPlaying: (arg0: boolean) => void) => {
   try {
-    await axios.post('http://localhost:3000/show/pause')
+    await axios.post(`${showUrl}/pause`);
     setPlaying(false);
-  } catch(error) {
-    console.log(`error asking api to play show`)
-    console.log(error)
+  } catch (error) {
+    console.log(`error asking api to play show`);
+    console.log(error);
   }
 };
