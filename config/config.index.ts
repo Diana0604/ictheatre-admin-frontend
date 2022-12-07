4154c68a337904cbcffc7c415cb5b685d0fcfdd0
Diana0604 <diana.vallverdu@gmail.com>
2022-12-07 09:07:27 +0000

    change baseurl

--- config/config.index.ts
+++ config/config.index.ts
@@ -1,8 +1,8 @@
 import { IConfig } from "../types/types.config";
 
 const config: IConfig = {
-  //baseUrl: `http://localhost:3000`, //baseurl where to ask to API
-  baseUrl: `http://192.168.100.1:3000`, //baseurl where to ask to API
+  baseUrl: `http://localhost:3000`, //baseurl where to ask to API
+  //baseUrl: `http://192.168.100.1:3000`, //baseurl where to ask to API
 };
 
 export default config;
